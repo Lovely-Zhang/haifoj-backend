9a91cbda86253c38edf9bf9a4ebd7ca36d038875
zhf <2764069380@qq.com>
2023-08-14 01:30:07 +0800

    后端项目代码初始化

---
+++ src/main/java/com/haif/oj/service/PostThumbService.java
@@ -0,0 +1,32 @@
+package com.haif.oj.service;
+
+import com.haif.oj.model.entity.PostThumb;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.haif.oj.model.entity.User;
+
+/**
+ * 帖子点赞服务
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface PostThumbService extends IService<PostThumb> {
+
+    /**
+     * 点赞
+     *
+     * @param postId
+     * @param loginUser
+     * @return
+     */
+    int doPostThumb(long postId, User loginUser);
+
+    /**
+     * 帖子点赞（内部服务）
+     *
+     * @param userId
+     * @param postId
+     * @return
+     */
+    int doPostThumbInner(long userId, long postId);
+}
